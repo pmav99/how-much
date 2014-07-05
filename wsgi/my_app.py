f84307bd8b598c931f56bb3a46a278da63ca5f59
Panos Mavrogiorgos <pmav99@gmail.com>
2014-07-06 00:52:52 +0300

    Update Flask application

--- wsgi/my_app.py
+++ wsgi/my_app.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request, render_template, url_for
+from analyzer import how_much
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def form():
+    return render_template('base.html')
+
+
+@app.route('/results/', methods=['POST'])
+def results():
+    url = request.form["query_url_name"]
+    results = how_much.main(url)
+    return "<code><pre> %s </pre></code>" % results
 
 
 if __name__ == "__main__":

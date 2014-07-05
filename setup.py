5fec45a0c0836b2b8ed2c4b2916d355915cbc07c
Panos Mavrogiorgos <pmav99@gmail.com>
2014-07-05 22:40:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+
+def read_lines(filename):
+    with open(filename, "rb", "utf-8") as f:
+        return f.readlines()
+
+
+setup(
+    name='bargain',
+    version='0.1.0',
+    description='Bargains from car.gr',
+    author='Panos Mavrogiorgos',
+    author_email='pmav99@gmail.com',
+    url='http://bargain-pmav99.rhcloud.com/',
+    install_requires=read_lines("requirements.txt")
+)
